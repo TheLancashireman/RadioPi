cea8883282038f5ac3f50128187eb83e5b26b943
dave <dave@5876d1cc-914d-45ba-88a3-d55f762e6afb>
2016-03-25 21:35:50 +0000

    Bit of progress
    git-svn-id: svn://lugh/srv/subversion/repositories/RadioPi@4 5876d1cc-914d-45ba-88a3-d55f762e6afb

--- controller/radiopi.py
+++ controller/radiopi.py
@@ -5,15 +5,17 @@
 import RPi.GPIO as GPIO
 import time
 from mpd import MPDClient
+import serial
 
-# Initialise the MPD client
+# Open port to LCD terminal (fixme: need to specify or discover)
+lcd = serial.Serial('/dev/ttyUSB0', 9600)
+lcd.write("\f")
+
+# Open connection to mpd
 mpdc = MPDClient()
 mpdc.timeout = 10
 mpdc.idletimeout = None
 mpdc.connect("localhost", 6600)
-
-mpdstatus = mpdc.status()
-volume = int(mpdstatus['volume'])
 
 # Initialise GPIO for the rotary encoder
 encoderA = 23       # BCM no. of Encoder 'A'
@@ -29,10 +31,12 @@ countMax = 10*scale-1
 countMin = 0
 
 # Initialise controller state
-modeVol = 1
-mode = modeVol
+modeHome = 1
+mode = modeHome
 
-# Callback for edge on channel A
+#===========================================================
+# EdgeA() - Callback for edge on channel A
+#===========================================================
 def edgeA(channel):
 	global countRaw
 	global countMax
@@ -59,18 +63,89 @@ def edgeA(channel):
 	if countRaw > countMax:
 		countRaw = countMin
 
+#===========================================================
 # Callback for edge on channel B
+#===========================================================
 def edgeB(channel):
 	global stateB
 	stateB = GPIO.input(encoderB)
 
+
+#===========================================================
+# HomeScreen() - read mpd status and display it
+#===========================================================
+
+g_vol = -1
+g_time = -1
+g_artist = ""
+g_album = ""
+g_title = ""
+
+lcd_go_artist	= "\033[1;1H"
+lcd_go_album	= "\033[2;1H"
+lcd_go_title	= "\033[3;1H"
+lcd_go_time		= "\033[4;1H"
+lcd_go_vol		= "\033[4;18H"
+lcd_clreol		= " "	# FIXME
+
+def HomeScreen():
+	global g_vol
+	global g_time
+	global g_artist
+	global g_album
+	global g_title
+
+	mpdstatus = mpdc.status()
+	l_vol = int(mpdstatus['volume'])
+	l_time = int(mpdstatus['time'])
+	mpdsong = mpdc.currentsong()
+	l_artist = mpdsong['artist']
+	l_album = mpdsong['album']
+	l_title = mpdsong['title']
+
+	if ( g_artist != l_artist ):
+		g_artist = l_artist
+		lcd.write(lcd_go_artist)
+		lcd.write(g_artist);
+		lcd.write(lcd_clreol)
+
+	if ( g_album != l_album ):
+		g_album = l_album
+		lcd.write(lcd_go_album)
+		lcd.write(g_album);
+		lcd.write(lcd_clreol)
+
+	if ( g_title != l_title ):
+		g_title = l_title
+		lcd.write(lcd_go_title)
+		lcd.write(g_title);
+		lcd.write(lcd_clreol)
+
+	if ( g_time != l_time ):
+		g_time = l_time
+		mins = l_time / 60
+		secs = l_time % 60
+		s_time = "%2d:%02d"%(mins, secs)
+		lcd.write(lcd_go_time)
+		lcd.write(s_time)
+		
+	if ( g_vol != l_vol ):
+		g_vol = l_vol
+		s_vol = "%3d"%(l_vol)
+		lcd.write(lcd_go_time)
+		lcd.write(s_vol)
+		
+		
+
+#=======================================
 GPIO.add_event_detect(encoderA, GPIO.BOTH, callback=edgeA, bouncetime=10)
 GPIO.add_event_detect(encoderB, GPIO.BOTH, callback=edgeB, bouncetime=10)
 
 try:
 	while 1:
-		time.sleep(10)
-# If CTRL+C is pressed, exit cleanly:
-except KeyboardInterrupt:
+		time.sleep(1)
+		if ( mode == modeHome ):
+			HomeScreen()
+except KeyboardInterrupt:		# If CTRL+C is pressed, exit cleanly:
 	GPIO.cleanup() # cleanup all GPIO
 	print ""
