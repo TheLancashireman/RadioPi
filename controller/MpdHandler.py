4c28e7918e86fc81f16ad55a133ef8f5ebb9c93b
dave <dave@5876d1cc-914d-45ba-88a3-d55f762e6afb>
2016-04-09 21:44:59 +0000

    First version of MpdHandler class Start of menu system design
    git-svn-id: svn://lugh/srv/subversion/repositories/RadioPi@14 5876d1cc-914d-45ba-88a3-d55f762e6afb

---
+++ controller/MpdHandler.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#
+# MpdHandler.py - event handler for MPD
+#
+# (c) David Haworth
+import mpd
+from mpd import MPDClient
+
+class MpdHandler:
+	def __init__(self):
+		self.mpdc = MPDClient()
+		self.mpdConnected = False
+		self.eventmap = {
+			'vol+'	: self.VolumeUp,
+			'vol-'	: self.VolumeDown,
+			'>/||'	: self.TogglePlayPause,
+			'stop'	: self.Stop,
+			'|<'	: self.SkipBack,
+			'>|'	: self.SkipForward,
+			'<<'	: self.SeekBack,
+			'>>'	: self.SeekForward	}
+
+		self.Connect()
+
+	def Connect():
+		try
+			self.mpdc.timeout = 10
+			self.mpdc.idletimeout = None
+			self.mpdc.connect("localhost", 6600)
+			self.mpdConnected = True
+		except mpd.ConnectionError:
+			self.mpdConnected = False
+
+	def VolumeUp():
+		s = mpdc.status()
+		v = int(s['volume'])
+		if v < 100:
+			mpdc.setvol(v+1)
+
+	def VolumeDown():
+		s = mpdc.status()
+		v = int(s['volume'])
+		if v > 0:
+			mpdc.setvol(v-1)
+
+	def TogglePlayPause():
+		mpdc.toggle()
+
+	def Stop():
+		mpdc.stop()
+
+	def SkipBack():
+		s = mpdc.status()
+		t = int(float(s['elapsed']))
+		if s < 5:
+			mpdc.previous()
+		else:
+			mpdc.seekcur(0)
+
+	def SkipForward():
+		mpdc.next()
+
+	def SeekBack():
+		mpdc.seekcur('-10')
+
+	def SeekForward():
+		mpdc.seekcur('+10')
+
+	# Event handler
+	def Event(evt):
+		if evt in self.eventmap.keys:
+			if not self.mpdConnected:
+				self.Connect()
+			if self.mpdConnected:
+				try:
+					self.eventmap[evt]()
+				except mpd.ConnectionError:
+					mpdConnected = False
+			return True
+		return False
