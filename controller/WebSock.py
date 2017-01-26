5aabd12758f4155516ee3d962ace1c3af7704aad
pi <pi@5876d1cc-914d-45ba-88a3-d55f762e6afb>
2017-01-26 18:20:29 +0000

    Ooops - broke the websocket handler. This puts it right.
    git-svn-id: svn://lugh/srv/subversion/repositories/RadioPi@59 5876d1cc-914d-45ba-88a3-d55f762e6afb

--- controller/WebSock.py
+++ controller/WebSock.py
@@ -10,13 +10,15 @@ from Config import radiopi_cfg
 from RadioPiLib import MountableDevs
 
 port_no = 6502
+evtq = 0
 
 valid_cmd = ['vol+', 'vol-', '>/||', 'stop', '|<', '>|', '<<', '>>', 'clear', 'shutdown', 'reboot', 'restart', 'restartmpd', 'umount']
 
 
 class WebSockHandler(WebSocket):
 	def __init__(self, eq):
-		self.eq = eq
+		global evtq
+		evtq = eq
 		print "Starting websocket server on localhost:%d" % port_no
 		self.server = SimpleWebSocketServer("", port_no, WebSock)
 		self.server.selectInterval = 0.0
@@ -29,19 +31,16 @@ class WebSockHandler(WebSocket):
 		return False
 
 class WebSock(WebSocket):
-	def __init__(self, eq):
-		self.eq = eq
-
 	def handleMessage(self):
 		msg = self.data
 		print "WebSock command:", msg
 		if msg in valid_cmd:
-			self.eq.PutEvent(msg)
+			evtq.PutEvent(msg)
 		elif msg[0:4] == "add ":
 			npath = os.path.normpath(os.path.join(radiopi_cfg.music_dir, msg[4:len(msg)]))
 			if npath[0:len(radiopi_cfg.music_dir)] == radiopi_cfg.music_dir:
 				if os.path.isfile(npath) or os.path.isdir(npath):
-					self.eq.PutEvent("add " + npath)
+					evtq.PutEvent("add " + npath)
 				else:
 					print "Websock invalid path:", msg
 			else:
@@ -50,7 +49,7 @@ class WebSock(WebSocket):
 			d = msg[6:len(msg)]
 			devs = MountableDevs("/dev")
 			if d in devs:
-				self.eq.PutEvent("mount " + os.path.join("/dev", d))
+				evtq.PutEvent("mount " + os.path.join("/dev", d))
 			else:
 				print "Websock invalid device:", msg
 		else:
