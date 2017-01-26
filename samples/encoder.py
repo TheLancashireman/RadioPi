2cb5bc9084247bfb813928775b2623375c80d6c3
pi <pi@5876d1cc-914d-45ba-88a3-d55f762e6afb>
2017-01-26 13:32:16 +0000

    Test code for new rotary encoder board
    git-svn-id: svn://lugh/srv/subversion/repositories/RadioPi@57 5876d1cc-914d-45ba-88a3-d55f762e6afb

--- samples/encoder.py
+++ samples/encoder.py
@@ -1,54 +1,70 @@
 #!/usr/bin/python
 #
-# An attempt to us a Panasonic rotary encoder connected to BCM23 and BCM24 of a raspberry pi
+# An attempt to use a rotary encoder connected to BCM23 and BCM24 of a raspberry pi
 
 import RPi.GPIO as GPIO
 import time
 
-encoderA = 23		# BCM no. of Encoder 'A'
-encoderB = 24		# BCM no. of Encoder 'B'
+encoderU = 4		# BCM no. of Encoder Up
+encoderD = 3		# BCM no. of Encoder Down
+encoderS = 2		# BCM no. of Switch
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(encoderA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoderB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(encoderU, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(encoderD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(encoderS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-stateB = False
-scale = 2
 count = 0
-countRaw = 0
-countMax = 10*scale-1
-countMin = 0
+stateU = 0
+stateD = 0
+stateS = 0
+countS = 0
 
-def edgeA(channel):
-		global countRaw
-		global countMax
-		global countMin
-		global count
-		global stateB
-		stateA = GPIO.input(encoderA)
-		if stateA == stateB:
-				countRaw = countRaw+1
-		else:
-				countRaw = countRaw-1
-		if countRaw < countMin:
-				countRaw = countMax
-		if countRaw > countMax:
-				countRaw = countMin
-		countX = countRaw/scale
-		if countX != count:
-				count = countX
-				print count
+def edgeU(channel):
+	global count
+	count = count + 1
+	print "+", count
 
-def edgeB(channel):
-		global stateB
-		stateB = GPIO.input(encoderB)
+def edgeD(channel):
+	global count
+	count = count - 1
+	print "-", count
 
-GPIO.add_event_detect(encoderA, GPIO.BOTH, callback=edgeA, bouncetime=10)
-GPIO.add_event_detect(encoderB, GPIO.BOTH, callback=edgeB, bouncetime=10)
+#GPIO.add_event_detect(encoderU, GPIO.RISING, callback=edgeU)
+#GPIO.add_event_detect(encoderD, GPIO.RISING, callback=edgeD)
 
 try:
-		while 1:
-				time.sleep(10)
+	while 1:
+		time.sleep(0.001)
+		s = GPIO.input(encoderU)
+		if ( s != stateU ):
+			stateU = s
+			if ( s ):
+				count = count + 1
+				print "+", count
+		s = GPIO.input(encoderD)
+		if ( s != stateD ):
+			stateD = s
+			if ( s ):
+				count = count - 1
+				print "-", count
+		s = GPIO.input(encoderS)
+		if ( s ):
+			if ( s == stateS ):
+				if ( countS < 250 ):
+					countS = countS + 1
+					if ( countS == 250 ):
+						print "L", count
+			else:
+				stateS = s
+				countS = 1
+		else:
+			if ( s != stateS and countS < 250 ):
+				print "S", count
+			stateS = s
+			countS = 0
+
+
 # If CTRL+C is pressed, exit cleanly:
 except KeyboardInterrupt:
 		GPIO.cleanup() # cleanup all GPIO
