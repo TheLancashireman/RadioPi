aadfbb4da85b0f6441e081e7540c09c94053196f
pi <pi@5876d1cc-914d-45ba-88a3-d55f762e6afb>
2017-01-26 18:21:05 +0000

    Test program for rotary encoder
    git-svn-id: svn://lugh/srv/subversion/repositories/RadioPi@60 5876d1cc-914d-45ba-88a3-d55f762e6afb

--- samples/encoder.py
+++ samples/encoder.py
@@ -14,28 +14,18 @@ GPIO.setup(encoderU, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(encoderD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(encoderS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+sleepyTime = 0.001
+longPressTime = 0.25
+longPressTicks = longPressTime/sleepyTime
 count = 0
 stateU = 0
 stateD = 0
 stateS = 0
 countS = 0
 
-def edgeU(channel):
-	global count
-	count = count + 1
-	print "+", count
-
-def edgeD(channel):
-	global count
-	count = count - 1
-	print "-", count
-
-#GPIO.add_event_detect(encoderU, GPIO.RISING, callback=edgeU)
-#GPIO.add_event_detect(encoderD, GPIO.RISING, callback=edgeD)
-
 try:
 	while 1:
-		time.sleep(0.001)
+		time.sleep(sleepyTime)
 		s = GPIO.input(encoderU)
 		if ( s != stateU ):
 			stateU = s
@@ -51,15 +41,15 @@ try:
 		s = GPIO.input(encoderS)
 		if ( s ):
 			if ( s == stateS ):
-				if ( countS < 250 ):
+				if ( countS < longPressTicks ):
 					countS = countS + 1
-					if ( countS == 250 ):
+					if ( countS >= longPressTicks ):
 						print "L", count
 			else:
 				stateS = s
 				countS = 1
 		else:
-			if ( s != stateS and countS < 250 ):
+			if ( s != stateS and countS < longPressTicks ):
 				print "S", count
 			stateS = s
 			countS = 0
